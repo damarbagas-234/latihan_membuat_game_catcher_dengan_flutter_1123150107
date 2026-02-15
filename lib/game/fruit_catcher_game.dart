c2ed23c9e274eeac1494dc766c7bfe486fdf7489
damarbagas-234 <muhammaddamar107@gmail.com>
2026-02-15 15:22:48 +0700

    mengumpullkan tugas

---
+++ lib/game/fruit_catcher_game.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+
+import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/widgets.dart';
+import 'package:game_project/game/components/basket.dart';
+import 'package:game_project/game/components/fruit.dart';
+import 'package:game_project/game/managers/audio_manager.dart';
+
+
+class FruitCatcherGame extends FlameGame
+    with PanDetector, HasCollisionDetection {
+  late Basket basket;
+  late TextComponent scoreText;
+  final Random random = Random();
+  double fruitSpawnTimer = 0;
+  final double fruitSpawnInterval = 1.5;
+
+  final ValueNotifier<int> scoreNotifier = ValueNotifier<int>(0);
+  int _score = 0;
+
+  int get score => _score;
+  set score(int value) {
+    _score = value;
+    scoreNotifier.value = value;
+  }
+
+  @override
+  Color backgroundColor() => const Color(0xFF87CEEB);
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    camera.viewport = FixedResolutionViewport(resolution: Vector2(400, 800));
+
+    basket = Basket();
+    await add(basket);
+
+    await AudioManager().initialize();
+    AudioManager().playBackgroundMusic();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    fruitSpawnTimer += dt;
+    if (fruitSpawnTimer >= fruitSpawnInterval) {
+      spawnFruit();
+      fruitSpawnTimer = 0;
+    }
+  }
+
+  void spawnFruit() {
+    final x = random.nextDouble() * size.x;
+    final fruit = Fruit(position: Vector2(x, -50));
+    add(fruit);
+  }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    basket.position.x += info.delta.global.x;
+    basket.position.x = basket.position.x.clamp(
+      basket.size.x / 2,
+      size.x - basket.size.x / 2,
+    );
+  }
+
+  void incrementScore() {
+    score++;
+    AudioManager().playSfx('collect.mp3');
+  }
+
+  void gameOver() {
+    AudioManager().playSfx('explode.mp3');
+    pauseEngine();
+  }
+
+  @override
+  void onRemove() {
+    AudioManager().pauseBackgroundMusic();
+    super.onRemove();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:game_project/game/fruit_catcher_game.dart';
+import 'package:game_project/game/managers/audio_manager.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await AudioManager().initialize();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Game', home: GameScreen());
+  }
+}
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  late FruitCatcherGame game;
+
+  @override
+  void initState() {
+    super.initState();
+    game = FruitCatcherGame();
+  }
+
+  @override
+  void dispose() {
+    game.onRemove();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          GameWidget(game: game),
+          Positioned(
+            top: 50,
+            left: 20,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ValueListenableBuilder<int>(
+                valueListenable: game.scoreNotifier,
+                builder: (context, score, child) {
+                  return Text(
+                    'Score: $score',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          Positioned(
+            top: 50,
+            right: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.music_note),
+                  onPressed: () {
+                    AudioManager().toggleMusic();
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.volume_up),
+                  onPressed: () {
+                    AudioManager().toggleSfx();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
